89f078858d442157703c242314b50b4c8f85b1fa
CricketXV <33155889+CricketXV@users.noreply.github.com>
2017-10-27 09:57:07 -0400

    SetupTheProject

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public Animator anim;
+    public Rigidbody2D rb;
+
+    public float speed = 200f;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("right"))
+        {
+            rb.AddForce(Vector2.right * speed * Time.deltaTime);
+            anim.Play("Walking");
+        }
+
+    }
+}
